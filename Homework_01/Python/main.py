7ccaac60a612af9024f63623f67a37881499379c
Stoyan Zlatev <47269030+Stoyan-Zlatev@users.noreply.github.com>
2024-10-16 19:06:06 +0300

    Update path printing

--- Homework_01/Python/main.py
+++ Homework_01/Python/main.py
@@ -1,13 +1,14 @@
 def dfs(space):
-    if found_solution[0] or tuple(initial_state) in seen:
+    tuple_initial_state = tuple(initial_state)
+    if found_solution[0] or tuple_initial_state in seen:
         return
 
-    path.append(initial_state[:])
-    seen.add(tuple(initial_state))
+    path.append(''.join(initial_state))
+    seen.add(tuple_initial_state)
 
-    if initial_state == target_state:
+    if tuple_initial_state == target_state:
         for p in path:
-            print(''.join(p))
+            print(p)
         found_solution[0] = True
         return
 
@@ -32,7 +33,7 @@ if __name__ == '__main__':
     N = int(input("Enter N: "))
     slots = 2 * N + 1
     initial_state = ['>'] * N + ['-'] + ['<'] * N
-    target_state = ['<'] * N + ['-'] + ['>'] * N
+    target_state = tuple(['<'] * N + ['-'] + ['>'] * N)
     seen = set()
     found_solution = [False]
     path = []
